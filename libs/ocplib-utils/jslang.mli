84f5cece21e7dfe66f33fb925e8fc751cdecf280
Fabrice Le Fessant <fabrice@lefessant.net>
2018-10-19 19:38:34 +0200

    Jslang.{ss_, t_, pcdata_t} to have lazy translations (#23)

--- libs/ocplib-utils/jslang.mli
+++ libs/ocplib-utils/jslang.mli
@@ -23,5 +23,11 @@ val set : ?set:storage -> StringCompat.StringMap.key -> unit
 val get : unit -> string option
 
 val s_ : string -> string
-val pcdata_s :
-  string -> [> `PCDATA ] Tyxml_js.Html5.elt
+val pcdata_s : string -> [> `PCDATA ] Tyxml_js.Html5.elt
+
+type string_id
+val ss_ : string -> string_id
+val t_ : string_id -> string
+val pcdata_t : string_id -> [> `PCDATA ] Tyxml_js.Html5.elt
+
+val string_ids : unit -> StringCompat.StringSet.t
