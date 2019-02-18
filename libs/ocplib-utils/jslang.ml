0762e5837fc6276e27fc1e212ea006e8a2c8622b
Cagdas Bozman <cagdas.bozman@ocamlpro.com>
2019-02-18 14:42:27 +0100

    Indent in jslang.ml

--- libs/ocplib-utils/jslang.ml
+++ libs/ocplib-utils/jslang.ml
@@ -12,9 +12,9 @@ let dictionaries = Hashtbl.create 512
 let current_dict = ref None
 
 module Local = JsStorage.MakeLocal(struct
-                             type t = string
-                             let name = localStorage_name
-                           end)
+    type t = string
+    let name = localStorage_name
+  end)
 
 let set ?(set=`Temp) lang =
   current_lang := Some lang;
@@ -22,12 +22,12 @@ let set ?(set=`Temp) lang =
   incr strings_epoch;
   match set with
   | `LocalStorage ->
-     Local.set lang;
-     Cookie.clear cookie_name
+    Local.set lang;
+    Cookie.clear cookie_name
   | `Temp -> ()
   | `Cookie ->
-     Local.clear ();
-     Cookie.set cookie_name lang
+    Local.clear ();
+    Cookie.set cookie_name lang
 
 let declare_table lang dict =
   Hashtbl.add dictionaries lang dict ;
@@ -55,18 +55,18 @@ let add_translations lang list =
 let () =
   match Jsloc.lang () with
   | Some lang ->
-     set ~set:`Cookie lang
+    set ~set:`Cookie lang
   | None ->
-  let cookies = Cookie.all () in
-  let lang =
-    try
-      Some (List.assoc cookie_name cookies)
-    with Not_found ->
-      Local.get ()
-  in
-  match lang with
-  | Some lang -> set lang
-  | None -> ()
+    let cookies = Cookie.all () in
+    let lang =
+      try
+        Some (List.assoc cookie_name cookies)
+      with Not_found ->
+        Local.get ()
+    in
+    match lang with
+    | Some lang -> set lang
+    | None -> ()
 
 let get () = !current_lang
 
@@ -75,14 +75,14 @@ let s_ s =
   match !current_dict with
   | None -> s
   | Some dict ->
-     try
-       Hashtbl.find dict s
-     with Not_found ->
-       match !current_lang with
-       | None -> assert false
-       | Some lang ->
-          Js_utils.log "Jslang: no %s-translation for %S" lang s;
-          s
+    try
+      Hashtbl.find dict s
+    with Not_found ->
+    match !current_lang with
+    | None -> assert false
+    | Some lang ->
+      Js_utils.log "Jslang: no %s-translation for %S" lang s;
+      s
 
 let txt_s s = Ocp_js.Html.txt (s_ s)
 let pcdata_s s = txt_s s
