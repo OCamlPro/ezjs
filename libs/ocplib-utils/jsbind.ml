5ed0fe243e7cd76c277f1a47eb630f5f2ea626a0
Max <max@maxdell.div.jussieu.fr>
2019-03-15 18:16:46 +0100

    different binding of amchart3

--- libs/ocplib-utils/jsbind.ml
+++ libs/ocplib-utils/jsbind.ml
@@ -6,28 +6,24 @@ type float_field = float Js.prop
 type bool_field = bool Js.prop
 type 'a array_field = 'a Js.js_array Js.t Js.prop
 
-let eval_js txt =
-  let _unit = Js.Unsafe.eval_string txt in
-  ()
+let eval_js txt = ignore(Js.Unsafe.eval_string txt)
 
 let link_js scriptUrl f =
-  Xhr.get "jsreq" scriptUrl
-          (fun txt ->
-            eval_js txt;
-            f ()
-          )
+  Xhr.get "jsreq" scriptUrl (fun txt ->
+      eval_js txt;
+      f ())
 
 let rec resolve_deps jsdeps baseurl f =
   match !jsdeps with
   | [] -> f ()
   | dep1 :: _tail ->
-     link_js (baseurl ^ dep1) (fun () ->
-             match !jsdeps with
-             | [] -> f ()
-             | dep2 :: tail ->
-                if dep1 = dep2 then begin
-                    jsdeps := tail;
-                    resolve_deps jsdeps baseurl f
-                  end
-                else
-                  f ())
+    link_js (baseurl ^ dep1) (fun () ->
+        match !jsdeps with
+        | [] -> f ()
+        | dep2 :: tail ->
+          if dep1 = dep2 then begin
+            jsdeps := tail;
+            resolve_deps jsdeps baseurl f
+          end
+          else
+            f ())
