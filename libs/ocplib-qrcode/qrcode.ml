b94041b0a59258ed34e5e2f589232b53fb4e2776
Cagdas Bozman <cagdas.bozman@ocamlpro.com>
2019-05-02 11:53:00 +0200

    Minimal qrcode binding (ocplib-qrcode)

---
+++ libs/ocplib-qrcode/qrcode.ml
@@ -0,0 +1,14 @@
+(** Minimal binding of http://davidshimjs.github.io/qrcodejs/ *)
+
+module Js = Js_of_ocaml.Js
+
+class type qrcode  = object
+  method makeCode : Js.js_string Js.t -> unit Js.meth
+end
+
+let make_code qrdiv text =
+  let qrcode_ctsr = Js.Unsafe.global##_QRCode in
+  let qrcode =
+  (Js.Unsafe.new_obj qrcode_ctsr
+     [| Js.Unsafe.inject qrdiv |] : qrcode Js.t) in
+  qrcode##makeCode (Js.string text)
