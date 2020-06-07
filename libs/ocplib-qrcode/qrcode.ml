2715d95ae0a401440c64897605bc6dd81a879c58
Maxime Levillain <maxime.levillain@origin-labs.com>
2020-06-07 18:21:18 +0200

    clear qrcode

--- libs/ocplib-qrcode/qrcode.ml
+++ libs/ocplib-qrcode/qrcode.ml
@@ -4,11 +4,14 @@ module Js = Js_of_ocaml.Js
 
 class type qrcode  = object
   method makeCode : Js.js_string Js.t -> unit Js.meth
+  method clear : unit Js.meth
 end
 
-let make_code qrdiv text =
-  let qrcode_ctsr = Js.Unsafe.global##._QRCode in
-  let qrcode =
-  (Js.Unsafe.new_obj qrcode_ctsr
-     [| Js.Unsafe.inject qrdiv |] : qrcode Js.t) in
-  qrcode##makeCode (Js.string text)
+type qrcode_cs = (Js.js_string Js.t -> Js.js_string Js.t -> qrcode Js.t) Js.constr
+
+let make_code id text =
+  let qrcode_ctsr : qrcode_cs = Js.Unsafe.global##._QRCode in
+  new%js qrcode_ctsr (Js.string id) (Js.string text)
+
+let update_code qrcode text = qrcode##makeCode (Js.string text)
+let clear_code qrcode = qrcode##clear
